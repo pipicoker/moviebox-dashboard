dc7c870fda2514c58d4b89940f5bacb3283f5cd5
princess Ekere <pipicoker@gmail.com>
2024-10-25 16:50:46 +0100

    adjusted widths for the progress bars

--- app/ui/movieDetails/AgeRange.tsx
+++ app/ui/movieDetails/AgeRange.tsx
@@ -7,44 +7,44 @@ const AgeRange = () => {
         <AudienceSubTopics topic='Age Range'/>
 
         <div className='mt-8'>
-          <div className=' flex  items-center justify-between  text-xs font-semibold'>
+          <div className=' grid grid-cols-10 space-x-2  items-center justify-between  text-xs font-semibold'>
                   
-                  <span className='w-1/12'>18-24</span>
-                  <div className=' h-1 bg-[#F5F5F5] w-9/12'>
+                  <span className='col-span-1'>18-24</span>
+                  <div className=' h-1 bg-[#F5F5F5] col-span-8'>
                     <div style={{width: `44.5%`}} className='h-1 bg-[#6A6A6A]  rounded-[40px]'  >
                     </div>
                   </div>
-                  <span className=" w-1/12">44.5%</span>
+                  <span className=" col-span-1">44.5%</span>
           </div>
 
-          <div className='mt-10 flex  items-center justify-between  text-xs font-semibold'>
+          <div className='mt-10 grid grid-cols-10 space-x-2  items-center justify-between  text-xs font-semibold'>
                   
-                  <span className='w-1/12'>25-34</span>
-                  <div className=' h-1 bg-[#F5F5F5] w-9/12'>
+                  <span className='col-span-1'>25-34</span>
+                  <div className=' h-1 bg-[#F5F5F5] col-span-8'>
                     <div style={{width: `47.1%`}} className='h-1 bg-gradient-to-r  from-[#FFA9A4]  via-[#8B9FE8]  to-[#35C3F3] rounded-[40px]'  >
                     </div>
                   </div>
-                  <span className=" w-1/12">47.1%</span>
+                  <span className=" col-span-1">47.1%</span>
           </div>
 
-          <div className='mt-10 flex  items-center justify-between text-xs font-semibold'>
+          <div className='mt-10 grid grid-cols-10 space-x-2  items-center justify-between text-xs font-semibold'>
                   
-                  <span className='w-1/12'>35-44</span>
-                  <div className=' h-1 bg-[#F5F5F5] w-9/12'>
+                  <span className='col-span-1'>35-44</span>
+                  <div className=' h-1 bg-[#F5F5F5] col-span-8'>
                     <div style={{width: `7.7%`}} className='h-1 bg-[#6A6A6A] rounded-[40px]'  >
                     </div>
                   </div>
-                  <span className=" w-1/12">7.7%</span>
+                  <span className="col-span-1">7.7%</span>
           </div>
 
-          <div className='mt-10 flex  items-center justify-between  text-xs font-semibold'>
+          <div className='mt-10 grid grid-cols-10 space-x-2  items-center justify-between  text-xs font-semibold'>
                   
-                  <span className='w-1/12'>45-54</span>
-                  <div className=' h-1 bg-[#F5F5F5] w-9/12'>
+                  <span className='col-span-1'>45-54</span>
+                  <div className=' h-1 bg-[#F5F5F5] col-span-8'>
                     <div style={{width: `0.5%`}} className='h-1 bg-[#6A6A6A] rounded-[40px]'  >
                     </div>
                   </div>
-                  <span className=" w-1/12">0.5%</span>
+                  <span className=" col-span-1">0.5%</span>
           </div>
         </div>
           
--- app/ui/movieDetails/TopLocations.tsx
+++ app/ui/movieDetails/TopLocations.tsx
@@ -6,44 +6,44 @@ const TopLocations = () => {
         <AudienceSubTopics topic='Top Locations'/>
 
         <div className='mt-8'>
-          <div className=' flex  items-center justify-between space-x-3 text-xs font-semibold'>
+          <div className=' grid grid-cols-9  items-center justify-between space-x-3 text-xs font-semibold'>
                   
-                  <label htmlFor="file" className="w-3/12">United States</label>
-                  <div className=' h-1 bg-[#F5F5F5] w-8/12'>
+                  <label htmlFor="file" className="col-span-2">United States</label>
+                  <div className=' h-1 bg-[#F5F5F5] col-span-6'>
                     <div style={{width: `44.5%`}} className='h-1 bg-[#6A6A6A] rounded-[40px]'  >
                     </div>
                   </div>
-                  <span className="w-1/12">44.5%</span>
+                  <span className="col-span-1">44.5%</span>
           </div>
 
-          <div className='mt-10 flex  items-center justify-between space-x-3 text-xs font-semibold'>
+          <div className='mt-10 grid grid-cols-9  items-center justify-between space-x-3 text-xs font-semibold'>
                   
-                  <label htmlFor="file" className="w-3/12">South Africa</label>
-                  <div className=' h-1 bg-[#F5F5F5] w-8/12'>
+                  <label htmlFor="file" className="col-span-2">South Africa</label>
+                  <div className=' h-1 bg-[#F5F5F5] col-span-6'>
                     <div style={{width: `47.1%`}} className='h-1 bg-gradient-to-r  from-[#FFA9A4]  via-[#8B9FE8]  to-[#35C3F3]  rounded-[40px]'  >
                     </div>
                   </div>
-                  <span className=" w-1/12">47.1%</span>
+                  <span className=" col-span-1">47.1%</span>
           </div>
 
-          <div className='mt-10 flex  items-center justify-between space-x-3 text-xs font-semibold'>
+          <div className='mt-10 grid grid-cols-9  items-center justify-between space-x-3 text-xs font-semibold'>
                   
-                  <label htmlFor="file" className="w-3/12">India</label>
-                  <div className=' h-1 bg-[#F5F5F5] w-8/12'>
+                  <label htmlFor="file" className="col-span-2">India</label>
+                  <div className=' h-1 bg-[#F5F5F5] col-span-6'>
                     <div style={{width: `7.7%`}} className='h-1 bg-[#6A6A6A] rounded-[40px]'  >
                     </div>
                   </div>
-                  <span className=" w-1/12">7.7%</span>
+                  <span className="col-span-1">7.7%</span>
           </div>
 
-          <div className='mt-10 flex  items-center justify-between space-x-3 text-xs font-semibold'>
+          <div className='mt-10 grid grid-cols-9  items-center justify-between space-x-3 text-xs font-semibold'>
                   
-                  <label htmlFor="file" className="w-3/12">London</label>
-                  <div className=' h-1 bg-[#F5F5F5] w-8/12'>
+                  <label htmlFor="file" className="col-span-2">London</label>
+                  <div className=' h-1 bg-[#F5F5F5] col-span-6'>
                     <div style={{width: `0.5%`}} className='h-1 bg-[#6A6A6A] rounded-[40px]'  >
                     </div>
                   </div>
-                  <span className="w-1/12 ">0.5%</span>
+                  <span className="col-span-1 ">0.5%</span>
           </div>
         </div>
           
